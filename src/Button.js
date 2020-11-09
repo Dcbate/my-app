1034499a998393c900b47717ddac1cc98b00286f
filoapp <56940444+filoapp@users.noreply.github.com>
2020-11-09 13:16:17 +0000

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -4,30 +4,30 @@ import React, { useState } from 'react';
 function Button() {
 
   const [showResults, setShowResults] = useState(false)
-  const RemoveDiv = () => setShowResults(false)
-  const AppearDiv = () => setShowResults(true)
+  const RemoveDiv = () => setShowResults(false) // minor one, wrap the func in {}, as without them it's implicit that you are returning from setShowResults -dont matter here cos its void anyway, but good practice
+  const AppearDiv = () => setShowResults(true) // you  dont need this one
 
 
-  const DIV = () => (
+  const DIV = () => ( // dont need this
     <div>
       Dave P Is a Mug
     </div>
   )
 
-  const DIV1 = () => (
+  const DIV1 = () => ( //dont need this
     <div>
       Dave B Is not a Mug
     </div>
   )
     return (
 <div class="centered">
-        <button onClick={AppearDiv}>
+        <button onClick={AppearDiv}> / dont need two separate parts here - one button only to delete
           Is Dave P a Mug?
           </button>
-          <button onClick={RemoveDiv}>
+          <button onClick={RemoveDiv}> // camelCase you mug
           Is Dave B a Mug?
           </button>
-          { showResults ? <DIV/> : <DIV1/> }
+          { showResults ? <DIV/> : <DIV1/> } // {showResults && <div>Dave is a mug</div>} -  when showResults is false the div is not rendered
         </div>
     );
 }
